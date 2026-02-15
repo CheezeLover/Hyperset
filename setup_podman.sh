fd59a495f5fc7aefeb2ecc306badb47dd173fd93
vincent-ginini <261498700+CheezeLover@users.noreply.github.com>
2026-02-15 10:58:59 +0100

    first update

---
+++ setup_podman.sh
@@ -0,0 +1,24 @@
+# Debian 12+
+sudo apt update && sudo apt install -y podman podman-compose
+
+# Check install
+podman --version
+podman-compose --version
+
+# Allow unpriviledged ports
+sudo sysctl -w net.ipv4.ip_unprivileged_port_start=80
+echo "net.ipv4.ip_unprivileged_port_start=80" | sudo tee /etc/sysctl.d/99-unprivileged-ports.conf
+
+# Create the network
+podman network create hyperset-net
+
+# Build custom caddy image with caddy security
+cd ~/Caddy
+podman build -t hyperset-caddy:latest .
+
+# Start Caddy
+cd ~/Caddy
+podman-compose up -d
+
+# Check logs
+podman-compose logs -f
