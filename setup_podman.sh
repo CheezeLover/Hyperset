d8265f6374fdef958b89611e30ebc7f15764d80a
CheezeLover <vincent@croise.eu>
2026-02-17 15:50:03 +0100

    Update setup_podman.sh

--- setup_podman.sh
+++ setup_podman.sh
@@ -1,11 +1,15 @@
 # Debian 12+
 sudo apt update && sudo apt install -y podman podman-compose
 
+# Install python libs
+sudo apt install python3-pip
+pip3 install psycopg2-binary
+
 # Check install
 podman --version
 podman-compose --version
 
-# Allow unpriviledged ports
+# Allow unpriviledged ports > OLD
 #sudo sysctl -w net.ipv4.ip_unprivileged_port_start=80
 #echo "net.ipv4.ip_unprivileged_port_start=80" | sudo tee /etc/sysctl.d/99-unprivileged-ports.conf
 
@@ -15,6 +19,10 @@ podman network create hyperset-net
 # Build custom caddy image with caddy security
 cd ~/Hyperset/Caddy
 podman build -t hyperset-caddy:latest .
+
+# Build custom image of Superset
+cd ~/Hyperset/Superset
+podman-compose build superset
 
 # Start Caddy
 cd ~/Hyperset
