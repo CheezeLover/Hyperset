63d8a924c715527262b7d38374acc9f96ff74b8d
CheezeLover <vincent@croise.eu>
2026-02-17 17:55:26 +0100

    test sql lite

--- Superset/superset_config.py
+++ Superset/superset_config.py
@@ -1,13 +1,10 @@
 import os
 
 SECRET_KEY = os.environ.get("SUPERSET_SECRET_KEY", "changeme")
-SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+SQLALCHEMY_DATABASE_URI = "sqlite:////app/superset_home/superset.db"
 
-# Critical for running behind a reverse proxy under /superset
 ENABLE_PROXY_FIX = True
 SESSION_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SAMESITE = "Lax"
 
-# Subpath config
 APPLICATION_ROOT = "/superset"
-WTF_CSRF_EXEMPT_LIST = []
