381057c4f3106df9b8f6c38a179d4188f46dcf56
CheezeLover <vincent@croise.eu>
2026-02-17 10:14:21 +0100

    Update setup_podman.sh

--- setup_podman.sh
+++ setup_podman.sh
@@ -6,8 +6,8 @@ podman --version
 podman-compose --version
 
 # Allow unpriviledged ports
-sudo sysctl -w net.ipv4.ip_unprivileged_port_start=80
-echo "net.ipv4.ip_unprivileged_port_start=80" | sudo tee /etc/sysctl.d/99-unprivileged-ports.conf
+#sudo sysctl -w net.ipv4.ip_unprivileged_port_start=80
+#echo "net.ipv4.ip_unprivileged_port_start=80" | sudo tee /etc/sysctl.d/99-unprivileged-ports.conf
 
 # Create the network
 podman network create hyperset-net
