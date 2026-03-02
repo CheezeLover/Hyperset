0d2182f9a128a6184aa94171441a82aa292fac67
vincent-ginini <261498700+CheezeLover@users.noreply.github.com>
2026-03-02 23:41:57 +0100

    good

--- Superset-Instance/superset_config_docker.py
+++ Superset-Instance/superset_config_docker.py
@@ -73,8 +73,8 @@ if _SUPERSET_THEME.get("enabled", True) and _SUPERSET_COLORS:
             # Background colors
             "colorBgBase": "#FFFFFF",
             "colorBgContainer": "#FFFFFF",
-            "colorBgElevated": "#FFFBF7",
-            "colorBgLayout": "#FFFBF7",
+            "colorBgElevated": "#FFFFFF",
+            "colorBgLayout": "#FFFFFF",
             
             # Text colors
             "colorText": "#1A202C",
@@ -82,8 +82,8 @@ if _SUPERSET_THEME.get("enabled", True) and _SUPERSET_COLORS:
             "colorTextTertiary": "#A0AEC0",
             
             # Border colors
-            "colorBorder": "#FFF5EE",
-            "colorBorderSecondary": "#FFF8F0",
+            "colorBorder": "#E5E5E5",
+            "colorBorderSecondary": "#F5F5F5",
             
             # Border radius
             "borderRadius": 8,
